a512db46f570c09ad64ad92af16f5ef95d148524
bhoromkar <118914116+bhoromkar@users.noreply.github.com>
2023-04-03 20:07:53 +0530

    second commit

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,7 +2,13 @@ package com.bridgelabz.snakeandladder;
 
 public class SnakeAndLadder {
     static int position = 0;
-    public static void main(String[] args) {
+    static void findNum() {
+        double value = (Math.random() * 10 % 6);
+        int diceValue = (int) value;
+        System.out.println(diceValue);
+    }
 
+    public static void main(String[] args) {
+findNum();
     }
 }
