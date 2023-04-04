e3f7ce93520534e70227b77a658ab5225d106e02
bhoromkar <118914116+bhoromkar@users.noreply.github.com>
2023-04-04 11:23:27 +0530

    fifth uc

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,43 +4,54 @@ public class SnakeAndLadder {
     static int position = 0;
     static int diceValue;
     static int diceMovement;
-    static int count;
+    static int count=0;
     static int dicePosition=0;
+    static int totalDicePosition;
+    //static int previousDicePosition;
+
 
    static final int FINAL_DICE_POSITION =100;
 
-    static void findNum() {
-      // int count;
+    static void diceRoll() {
 
-        while (dicePosition <= FINAL_DICE_POSITION) {
+
+        while (dicePosition < FINAL_DICE_POSITION ) {
             double diceMove = (Math.random() * 10 % 3);
             diceMovement = (int) diceMove;
             double value = (Math.random() * 10 % 6);
             diceValue = (int) value;
             System.out.println(diceValue);
             System.out.println(diceMovement);
+           // previousDicePosition = dicePosition;
             switch (diceMovement) {
                 case 1:
                     System.out.println("Player Move Forward");
-                    dicePosition = dicePosition + diceValue;
+                    if((dicePosition + diceValue) <= FINAL_DICE_POSITION)
+                        dicePosition = dicePosition + diceValue;
                     break;
-                case 2 :
+                case 2:
+
                     System.out.println("Player Move Backward");
                     dicePosition = dicePosition - diceValue;
+                    if (dicePosition < 0)
+                        dicePosition = 0;
+
                     break;
                 default:
                     System.out.println("No Play");
 
             }
-
-
+                count++;
+                System.out.println("Position of dice is: " + dicePosition);
         }
 
-            System.out.println(dicePosition);
+            System.out.println(count + " times dice roll.");
         }
 
         public static void main (String[]args){
-            findNum();
+            
         }
     }
+
+
 
