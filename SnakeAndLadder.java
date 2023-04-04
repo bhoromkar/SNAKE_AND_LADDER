35a8ba253aa4bd3d810bf2f4ab8e289b9aa14ba7
bhoromkar <118914116+bhoromkar@users.noreply.github.com>
2023-04-04 09:28:19 +0530

    fouth uc

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,14 +5,14 @@ public class SnakeAndLadder {
     static int diceValue;
     static int diceMovement;
     static int count;
-    static int dicePosition;
+    static int dicePosition=0;
 
-   // static final int FINAL_DICE_MOVEMENT
+   static final int FINAL_DICE_POSITION =100;
 
     static void findNum() {
-       // int count;
+      // int count;
 
-        while (dicePosition >= 100) {
+        while (dicePosition <= FINAL_DICE_POSITION) {
             double diceMove = (Math.random() * 10 % 3);
             diceMovement = (int) diceMove;
             double value = (Math.random() * 10 % 6);
@@ -21,17 +21,18 @@ public class SnakeAndLadder {
             System.out.println(diceMovement);
             switch (diceMovement) {
                 case 1:
-                    System.out.println("No Play");
-                    break;
-                case 2:
-                    //dicePosition = dicePosition + diceValue;
                     System.out.println("Player Move Forward");
+                    dicePosition = dicePosition + diceValue;
                     break;
-                case 3:
-                    //dicePosition = dicePosition - diceValue;
+                case 2 :
                     System.out.println("Player Move Backward");
+                    dicePosition = dicePosition - diceValue;
+                    break;
+                default:
+                    System.out.println("No Play");
 
             }
+
 
         }
 
