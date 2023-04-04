bc1ed5fe2dedbef55ecbf9e6e0e78ff3f0fc4b38
bhoromkar <118914116+bhoromkar@users.noreply.github.com>
2023-04-04 09:15:44 +0530

    third uc

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,13 +2,44 @@ package com.bridgelabz.snakeandladder;
 
 public class SnakeAndLadder {
     static int position = 0;
+    static int diceValue;
+    static int diceMovement;
+    static int count;
+    static int dicePosition;
+
+   // static final int FINAL_DICE_MOVEMENT
+
     static void findNum() {
-        double value = (Math.random() * 10 % 6);
-        int diceValue = (int) value;
-        System.out.println(diceValue);
+       // int count;
+
+        while (dicePosition >= 100) {
+            double diceMove = (Math.random() * 10 % 3);
+            diceMovement = (int) diceMove;
+            double value = (Math.random() * 10 % 6);
+            diceValue = (int) value;
+            System.out.println(diceValue);
+            System.out.println(diceMovement);
+            switch (diceMovement) {
+                case 1:
+                    System.out.println("No Play");
+                    break;
+                case 2:
+                    //dicePosition = dicePosition + diceValue;
+                    System.out.println("Player Move Forward");
+                    break;
+                case 3:
+                    //dicePosition = dicePosition - diceValue;
+                    System.out.println("Player Move Backward");
+
+            }
+
+        }
+
+            System.out.println(dicePosition);
+        }
+
+        public static void main (String[]args){
+            findNum();
+        }
     }
 
-    public static void main(String[] args) {
-findNum();
-    }
-}
